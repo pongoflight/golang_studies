96f5ae8b34e1840d8b67c559e01a0e06b5a4b17e
wlx <pongoflight@hotmail.com>
2012-11-25 13:46:04 +0800

    add 1.3:newgo print self.

---
+++ 1.3/newgo.go
@@ -0,0 +1,25 @@
+/*   */
+package main
+import ( 
+    "fmt"
+)
+
+var ()
+const ()
+func f() {}
+func main() {
+    fmt.Println(s)
+}
+const s = `/*   */
+package main()
+import (
+    "fmt"
+)
+
+var ()
+const ()
+func f() {}
+func main() {
+    fmt.Println(s)
+}
+`
